6ec621813017061856a7d06ccf88c6c688933e3f
Aarav <pl46u3.ducklinz@gmail.com>
2023-02-10 22:17:10 +0530

    bug fixes and updates

--- __init__.py
+++ __init__.py
@@ -1,10 +1,17 @@
+"""
+Import necessary libraries
+"""
 from wsgiref.validate import validator as vd
 from wsgiref.simple_server import make_server as ms
 import json,traceback
 
 paths={}
+headerz=[]
 
 def custom_parser(data):
+    """
+    For parsing data in json.
+    """
     funcs=[json.loads]
     for x in funcs:
         try:
@@ -14,9 +21,6 @@ def custom_parser(data):
     return str(data)
 
 def app(env, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html'),('Access-Control-Allow-Origin', '*')] 
-    start_response(status, headers)
     path=env["PATH_INFO"]
     qs=env["QUERY_STRING"].split("&")
     if "" in qs:qs.remove("")
@@ -31,26 +35,44 @@ def app(env, start_response):
             pass
     res=b""
     temp_res=b""
+    status = '200 OK'
     if path in paths:
         try:
-            temp_res=paths[path](qs)
+            temp_res=paths[path](qs,env)
         except:
+            import traceback
             traceback.print_exc()
+            status = '404 Not Found'
         if temp_res!=b"":
             if type(temp_res)==type(""):
                 res=temp_res.encode()
-            if type(temp_res)==type(b""):
+            elif type(temp_res)==type(b""):
                 res=temp_res
-            if type(temp_res) in [type(1.0),type(1),type([]),type({})]:
+            elif type(temp_res) in [type(1.0),type(1),type([]),type({})]:
                 try:
                     res=json.dumps(temp_res).encode()
                 except:
-                    print(";/")
+                    status = '404 Not Found'
+            else:
+                status = '404 Not Found'
+    start_response(status, headerz)
     return [res]
 
-def start_server(port,routes):
-    global paths
+def start_server(port=8080,routes={},headers=[('Content-type', 'text/text'),('Access-Control-Allow-Origin', '*')] ):
+    """
+    This functions starts a http rest api.
+    # Arguments
+    port: int (default 8080),
+    routes: {path:function(query_string,env)} (default {}),
+    headers: [(name,value),] (default [('Content-type', 'text/text'),('Access-Control-Allow-Origin', '*')])
+    The functions for routes will be called when the api gets a request for the specified end point.
+    # Function arguments
+    i. Query String (dictionary)
+    ii. Environment - Request Details (dictionary)
+    """
+    global paths,headerz
     paths=routes
+    headerz=headers
     validator_app = vd(app)
     with ms('', port, validator_app) as httpd:
         httpd.serve_forever()
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Foobar
+
+Joshua is a light-weight python wsgi
+
+## Installation
+Inside of project
+```bash
+git clone https://github.com/aludayalu/joshua
+```
+## Usage
+
+```python
+import joshua
+
+def home_page(query_string,env):
+    return {"data":"Hello World!"}
+
+joshua.start_server(port=8080,routes={"/":home_page})
+```
